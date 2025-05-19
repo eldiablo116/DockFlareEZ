3b3ad0c8af850bfbf7ced91151639e3cca6468a5
eldiablo116 <143644185+eldiablo116@users.noreply.github.com>
2025-05-19 11:56:26 +0100

    Update DockFlareEZsetup.sh

--- DockFlareEZsetup.sh
+++ DockFlareEZsetup.sh
@@ -8,7 +8,7 @@ RESET='\e[0m'
 
 # --- Branding ---
 PREFIX="$(echo -e "${BLUE}[Dock${ORANGE}Flare${GREEN}EZ${RESET}]")"
-echo -e "${ORANGE}===============================\n   DockFlare EZSetup v5.4h\n===============================${RESET}\n"
+echo -e "${ORANGE}===============================\n   DockFlare EZSetup v5.5\n===============================${RESET}\n"
 
 # --- Reusable Function: Prompt for DNS Record ---
 create_dns_record_prompt() {
@@ -438,13 +438,13 @@ services:
 
       # HTTP Router
       traefik.http.routers.portainer-http.entrypoints: web
-      traefik.http.routers.portainer-http.rule: Host(\`portainer.${CF_ZONE}\`)
+      traefik.http.routers.portainer-http.rule: "Host(\"portainer.${CF_ZONE}\")"
       traefik.http.routers.portainer-http.middlewares: globalHeaders@file,redirect-to-https@docker,robotHeaders@file,cloudflarewarp@file
       traefik.http.routers.portainer-http.service: portainer
 
       # HTTPS Router
       traefik.http.routers.portainer.entrypoints: websecure
-      traefik.http.routers.portainer.rule: Host(\`portainer.${CF_ZONE}\`)
+      traefik.http.routers.portainer.rule: "Host(\"portainer.${CF_ZONE}\")"
       traefik.http.routers.portainer.middlewares: globalHeaders@file,secureHeaders@file,robotHeaders@file,cloudflarewarp@file
       traefik.http.routers.portainer.tls.certresolver: cloudflare
       traefik.http.routers.portainer.tls.options: securetls@file
