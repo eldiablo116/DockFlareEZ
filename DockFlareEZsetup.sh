76eb2cad4bba3e7f15d015cdffbc064c4a8b8f5a
eldiablo116 <143644185+eldiablo116@users.noreply.github.com>
2025-05-19 08:51:48 +0100

    Update DockFlareEZsetup.sh

--- DockFlareEZsetup.sh
+++ DockFlareEZsetup.sh
@@ -8,7 +8,7 @@ RESET='\e[0m'
 
 # --- Branding ---
 PREFIX="$(echo -e "${BLUE}[Dock${ORANGE}Flare${GREEN}EZ${RESET}]")"
-echo -e "${ORANGE}===============================\n   DockFlare EZSetup v3.2\n===============================${RESET}\n"
+echo -e "${ORANGE}===============================\n   DockFlare EZSetup v3.3\n===============================${RESET}\n"
 
 # --- Track Success Flags ---
 UPDATE_OK=false
